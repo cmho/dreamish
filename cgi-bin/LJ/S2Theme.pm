a4d7877ccdf6506bae36e8a5f1a003ba61b35eea
Carly Ho <carlymho@fastmail.com>
2024-03-17 00:44:58 -0500

    add to theme list

--- cgi-bin/LJ/S2Theme.pm
+++ cgi-bin/LJ/S2Theme.pm
@@ -51,6 +51,7 @@ sub default_themes {
         drifting         => 'drifting/blue',
         dustyfoot        => 'dustyfoot/dreamer',
         easyread         => 'easyread/green',
+        generations      => 'generations/default',
         goldleaf         => 'goldleaf/elegantnotebook',
         fantaisie        => 'fantaisie/unrelentingroutine',
         fiveam           => 'fiveam/earlyedition',
